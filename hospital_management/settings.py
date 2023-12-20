df8863b53420abf3446fa6a7826e66ac8ba6e4b9
abdullahallnaim <naim1823007@stud.kuet.ac.bd>
2023-12-20 18:51:23 +0600

    done

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -133,9 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
